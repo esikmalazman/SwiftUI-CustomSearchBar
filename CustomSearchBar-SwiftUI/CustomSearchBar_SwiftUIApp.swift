ec347c510936cc2afef42b9d294bb88737ae6eae
Ikmal Azman <opikmalazman@gmail.com>
2023-04-17 14:30:08 +0100

    Initial Commit

---
+++ CustomSearchBar-SwiftUI/CustomSearchBar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSearchBar_SwiftUIApp.swift
+//  CustomSearchBar-SwiftUI
+//
+//  Created by Ikmal Azman on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomSearchBar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
